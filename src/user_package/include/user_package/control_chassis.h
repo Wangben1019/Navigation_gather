121a7468b9b44669a6bee5ba5c3bb6137f029dcb
wangben <2782760102@qq.com>
2024-07-29 14:58:28 +0800

    add joy

--- src/user_package/include/user_package/control_chassis.h
+++ src/user_package/include/user_package/control_chassis.h
@@ -15,4 +15,12 @@ typedef struct{
     double Expect_Speed_Yaw;
 } Robot_Chassis_Exp_;
 
+typedef struct{
+    double remote_speed_x;
+    double remote_speed_y;
+    double remote_speed_yaw;
+    double switch_remote;
+    int    remote_stop_flag; 
+} Remote_Chassis_Exp_;
+
 #endif
--- src/user_package/src/control_chassis.cpp
+++ src/user_package/src/control_chassis.cpp
@@ -11,6 +11,7 @@
 #include <memory>
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/TwistStamped.h>
+#include <sensor_msgs/Joy.h>
 
 #include <message_filters/subscriber.h>
 #include <message_filters/sync_policies/approximate_time.h>
@@ -20,6 +21,8 @@
 #include <boost/make_shared.hpp>
 
 bool Navigation_Mode_now = true;
+
+sensor_msgs::Joy joy_laser;
 
 // std::shared_ptr<control_chassis_ns::control_chassis> vcom_trinsmit_speed(new control_chassis_ns::control_chassis());
 void OdometryAndCmd_VelHandler(const geometry_msgs::TwistStamped::ConstPtr& cmd_vel)
@@ -31,34 +34,39 @@ void OdometryAndCmd_VelHandler(const geometry_msgs::TwistStamped::ConstPtr& cmd_
     Robot_Chassis_Exp.Expect_Speed_X = cmd_vel->twist.linear.x;
     Robot_Chassis_Exp.Expect_Speed_Y = cmd_vel->twist.linear.y;
     Robot_Chassis_Exp.Expect_Speed_Yaw = cmd_vel->twist.angular.z;
+
     // robot_expect = (Robot_Chassis_Exp_ *)serial_data_user.data();
     serial_data_user.append((char *)&Robot_Chassis_Exp, sizeof(Robot_Chassis_Exp_));
-    ROS_INFO("X = %f,Y = %f,YAW = %f\n", Robot_Chassis_Exp.Expect_Speed_X, Robot_Chassis_Exp.Expect_Speed_Y, Robot_Chassis_Exp.Expect_Speed_Yaw);
+    ROS_INFO("X = %f,Y = %f,YAW = %f", Robot_Chassis_Exp.Expect_Speed_X, Robot_Chassis_Exp.Expect_Speed_Y, Robot_Chassis_Exp.Expect_Speed_Yaw);
 
-    ROS_INFO("Robot is in navigation mode");
+    // ROS_INFO("Robot is in navigation mode");
 
     vcom_user.Transmit(1, 1, serial_data_user);
 }
 
-void cmd_vel_user_callback(const geometry_msgs::TwistStamped::ConstPtr& cmd_vel)
+void joystickHandler(const sensor_msgs::Joy::ConstPtr& joy)
 {
-    Robot_Chassis_Exp_ Robot_Chassis_Exp;
+    joy_laser = *joy;
+    Remote_Chassis_Exp_ Robot_Chassis_Exp;
     VCOMCOMM vcom_user;
     QByteArray serial_data_user;
-    
-    Robot_Chassis_Exp.Expect_Speed_X = cmd_vel->twist.linear.x;
-    Robot_Chassis_Exp.Expect_Speed_Y = cmd_vel->twist.linear.y;
-    Robot_Chassis_Exp.Expect_Speed_Yaw = cmd_vel->twist.angular.z;
 
+    Robot_Chassis_Exp.remote_speed_x = joy_laser.axes[4];
+    Robot_Chassis_Exp.remote_speed_y = joy_laser.axes[3];
+    Robot_Chassis_Exp.remote_speed_yaw = joy_laser.axes[0];
+    Robot_Chassis_Exp.switch_remote = joy_laser.axes[2];
+    Robot_Chassis_Exp.remote_stop_flag = joy_laser.buttons[4];
+
+
+    // robot_expect = (Robot_Chassis_Exp_ *)serial_data_user.data();
     serial_data_user.append((char *)&Robot_Chassis_Exp, sizeof(Robot_Chassis_Exp_));
+    ROS_INFO("X = %f,Y = %f,YAW = %f", Robot_Chassis_Exp.remote_speed_x, Robot_Chassis_Exp.remote_speed_y, Robot_Chassis_Exp.remote_speed_yaw);
+    ROS_INFO("stop = %d,switch = %f", Robot_Chassis_Exp.remote_stop_flag, Robot_Chassis_Exp.switch_remote);
 
-    ROS_INFO("X = %f,Y = %f,YAW = %f\n", Robot_Chassis_Exp.Expect_Speed_X, Robot_Chassis_Exp.Expect_Speed_Y, Robot_Chassis_Exp.Expect_Speed_Yaw);
+    // ROS_INFO("Robot is in navigation mode");
 
-    ROS_INFO("Robot is not in navigation mode");
-
-    vcom_user.Transmit(1, 1, serial_data_user);
+    vcom_user.Transmit(2, 2, serial_data_user);
 }
-
 int main(int argc, char *argv[])
 {
     ros::init(argc, argv, "control_chassis");
@@ -66,9 +74,9 @@ int main(int argc, char *argv[])
 
     nh.getParam("Navigation_Mode_now", Navigation_Mode_now);
 
-    ros::Subscriber subCmd_Vel = Navigation_Mode_now ? \
-     nh.subscribe<geometry_msgs::TwistStamped>("/cmd_vel", 10, OdometryAndCmd_VelHandler) : \
-     nh.subscribe<geometry_msgs::TwistStamped>("/cmd_vel_user", 10, cmd_vel_user_callback);
+    ros::Subscriber subCmd_Vel = nh.subscribe<geometry_msgs::TwistStamped>("/cmd_vel", 10, OdometryAndCmd_VelHandler);
+
+    ros::Subscriber subJoystick = nh.subscribe<sensor_msgs::Joy> ("/joy", 5, joystickHandler);
 
     ROS_INFO("%d", Navigation_Mode_now);
 
